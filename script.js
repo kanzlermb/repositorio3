08f6e77a1f4a9fde3401fda04a0cd095dfff794d
kanzlerxm <143618182+kanzlermb@users.noreply.github.com>
2025-04-07 22:52:51 -0300

    salvo

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const aumentaFonteBotao = document.getElementById('aumentarfonte');
+    aumentaFonteBotao.addEventListener('click', function () {
+
+    })
+    const botaoDeAcessibilidade = document.getElementById
+        ('botao-acessibilidade');
+        cosnt opcoesDeAcessibilidade = document.getElementById
+        ('opcoes-acessibilidade');
+})   
